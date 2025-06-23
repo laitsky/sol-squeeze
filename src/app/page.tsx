9ac2cb3b3f53fbc5f3c0e1607bc186e02988f3c6
laitsky <vincent.diamond15@gmail.com>
2025-06-23 20:15:48 +0700

    feat: show all users connected token

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ import dynamic from 'next/dynamic'
 
 import { useState, useEffect } from 'react'
 import { useWallet } from '@solana/wallet-adapter-react'
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
-import { Connection, PublicKey } from '@solana/web3.js'
+import { Connection, GetProgramAccountsFilter, PublicKey } from '@solana/web3.js'
 import { TOKEN_PROGRAM_ID } from '@solana/spl-token'
 import { Box, VStack, Heading, Text, List, ListItem } from '@chakra-ui/react'
 
@@ -19,32 +18,63 @@ export default function Home() {
 
   useEffect(() => {
     if (connected && publicKey) {
-      fetchTokens()
+      console.log("public address: ", publicKey.toBase58())
+      fetchTokens(publicKey)
+    } else {
+      setTokens([])
     }
   }, [connected, publicKey])
 
-  async function fetchTokens() {
-    const connection = new Connection('https://api.mainnet-beta.solana.com')
-    const tokenAccounts = await connection.getParsedTokenAccountsByOwner(
-      publicKey as PublicKey,
+  async function fetchTokens(publicKey: PublicKey) {
+    const connection = new Connection(process.env.NEXT_PUBLIC_SOLANA_RPC_URL as string)
+
+    const filters: GetProgramAccountsFilter[] = [
       {
-        programId: TOKEN_PROGRAM_ID,
+        dataSize: 165,
+      },
+      {
+        memcmp: {
+          offset: 32,
+          bytes: publicKey.toString()
+        }
       }
-    )
+    ];
 
-    const tokenList = tokenAccounts.value.map((accountInfo) => ({
-      mint: accountInfo.account.data.parsed.info.mint,
-      amount: accountInfo.account.data.parsed.info.tokenAmount.uiAmount,
-    }))
+    try {
+      const accounts = await connection.getParsedProgramAccounts(TOKEN_PROGRAM_ID, { filters })
+      console.log(`Found ${accounts.length} token account(s) for wallet ${publicKey.toBase58()}.`);
 
-    setTokens(tokenList)
+      const fetchedTokens: Token[] = accounts.map((account, i) => {
+        // Parse the account data
+        const parsedAccountInfo: any = account.account.data;
+        const mintAddress: string = parsedAccountInfo["parsed"]["info"]["mint"];
+        const tokenBalance: number = parsedAccountInfo["parsed"]["info"]["tokenAmount"]["uiAmount"];
+        // Log results
+        console.log(`Token Account No. ${i + 1}: ${account.pubkey.toString()}`);
+        console.log(`--Token Mint: ${mintAddress}`);
+        console.log(`--Token Balance: ${tokenBalance}`);
+        return { mint: mintAddress, amount: tokenBalance };
+      });
+
+      setTokens(fetchedTokens);
+    } catch (error) {
+        console.error('Error fetching tokens:', error);
+    }
   }
+
+  const WalletMultiButtonDynamic = dynamic(
+    () => import('@solana/wallet-adapter-react-ui').then((mod) => mod.WalletMultiButton),
+    {
+      ssr: false,
+      loading: () => <div className="wallet-loader">Loading wallet...</div>
+    }
+  )
 
   return (
     <Box p={8}>
       <VStack spacing={8} align="stretch">
         <Heading>SPL Token List</Heading>
-        <WalletMultiButton />
+        <WalletMultiButtonDynamic />
         {connected ? (
           <List spacing={3}>
             {tokens.map((token, index) => (
